74cd5ae3399874eb9b4505cdd1d339235143d0fe
Isaias Castaneda <97637558+Icastaneda270@users.noreply.github.com>
2025-08-02 16:49:29 -0700

    Updated names in PATIENTS table.
    Update PATIENTS table to remove numbers from "FIRST", "LAST", "MAIDEN".

---
+++ Queries/Update_Patients_Table.sql
@@ -0,0 +1,29 @@
+-- Test Table
+SELECT * FROM hospital_db.patients;
+-- Results Example: First = Lavonia8, Last = Wunsch504, Maiden = Will178
+
+-- Updating table to remove numbers from 
+SELECT 
+    FIRST AS original_first,
+    REGEXP_REPLACE(FIRST, '[0-9]', '') AS cleaned_first,
+    LAST AS original_last,
+    REGEXP_REPLACE(LAST, '[0-9]', '') AS cleaned_last,
+    MAIDEN AS original_maiden,
+    REGEXP_REPLACE(MAIDEN, '[0-9]', '') AS cleaned_maiden
+FROM 
+    hospital_db.patients
+WHERE 
+    FIRST REGEXP '[0-9]' 
+    OR LAST REGEXP '[0-9]'
+    OR MAIDEN REGEXP '[0-9]';
+
+-- Once verified, run the update
+UPDATE hospital_db.patients
+SET 
+    FIRST = REGEXP_REPLACE(FIRST, '[0-9]', ''),
+    LAST = REGEXP_REPLACE(LAST, '[0-9]', ''),
+    MAIDEN = REGEXP_REPLACE(MAIDEN, '[0-9]', '')
+WHERE 
+    FIRST REGEXP '[0-9]' 
+    OR LAST REGEXP '[0-9]'
+    OR MAIDEN REGEXP '[0-9]';
